8464526e65b115fccc4f49056f21f461c14582c0
gagan <gagankhurana2006@gmail.com>
2025-08-06 11:37:02 +0530

    feat: Implement encapsulation and basic attendance recording (Part 4)

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -4,13 +4,23 @@ package src.com.school;
 public class Course {
     private static int nextCourseIdCounter = 101; // Start course IDs from 101 and made private
 
-    int courseId; // Changed to int
-    String courseName;
+    private int courseId; // Changed to int
+     private String courseName;
 
     // Constructor
     public Course(String courseName) {
         this.courseId = nextCourseIdCounter++; // Auto-increment and assign ID
         this.courseName = courseName;          // Assign course name
+    }
+
+    //Getter for courseId
+    public int getCourseId(){
+        return courseId;
+    }
+
+    //Getter for courseName 
+    public String getCourseName(){
+        return courseName;
     }
 
     public void displayDetails() {
